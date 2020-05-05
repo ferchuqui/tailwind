dcfa3106063290bac570c34f61b925263ac94e57
ferchuqui <luisfernandofarji@hotmail.com>
2020-05-05 10:00:06 -0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
-    },
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
